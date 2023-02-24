6ae90a3915d028040638d7cf39355da73cfd9e68
Documenter.jl <documenter@juliadocs.github.io>
2023-02-24 19:30:12 +0000

    build based on 7f03ca3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ConvexHulls2d","category":"page"},{"location":"#ConvexHulls2d","page":"Home","title":"ConvexHulls2d","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ConvexHulls2d.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ConvexHulls2d]","category":"page"}]
+}
